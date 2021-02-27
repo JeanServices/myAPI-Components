5275cad695d35add07ce3894c0867a9b41b144ec
Jean <46204992+JeanCarlosVR@users.noreply.github.com>
2021-02-27 13:15:53 -0500

    Update routes.go

--- routes.go
+++ routes.go
@@ -9,23 +9,22 @@ import (
 func RegisterRoutes(server *fiber.App, client *mongo.Client) {
 	server.Use("/api/v1", func(c *fiber.Ctx) error {
 		c.Accepts("application/json")
+		return c.Next()
+	})
 
-	   return c.Next()
-   })
-
-   server.Get("/api/v1", func(c *fiber.Ctx) error {
+	server.Get("/api/v1", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
 			"status": 200,
 			"message": "ok",
 			"exited_code": 0,
 		})
-   })
+	})
 
-   server.Post("/api/v1/create", func(c *fiber.Ctx) error {
-	   fmt.Println(c.Body())
+	server.Post("/api/v1/create", func(c *fiber.Ctx) error {
+		fmt.Println(c.Body())
 
-	   return c.JSON(fiber.Map{
-		   "jean": "jean",
-	   })
-   })
+		return c.JSON(fiber.Map{
+			"jean": "jean",
+		})
+	})
 }
