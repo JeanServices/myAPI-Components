0722a14424bb0aa22e215824394b9505661e488b
Jean <46204992+JeanCarlosVR@users.noreply.github.com>
2021-02-27 13:33:21 -0500

    Update routes.go

--- routes.go
+++ routes.go
@@ -6,6 +6,11 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+type CreateUser struct {
+	ID
+	Name
+}
+
 func RegisterRoutes(server *fiber.App, client *mongo.Client) {
 	server.Use("/api/v1", func(c *fiber.Ctx) error {
 		c.Accepts("application/json")
@@ -21,6 +26,7 @@ func RegisterRoutes(server *fiber.App, client *mongo.Client) {
 	})
 
 	server.Post("/api/v1/create", func(c *fiber.Ctx) error {
+		err := fiber.BodyParser(&CreateUser)
 		fmt.Println(c.Body())
 
 		return c.JSON(fiber.Map{
